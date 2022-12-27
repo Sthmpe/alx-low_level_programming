cdc59e2ddd830b3f3ab749ecf0eb2a117a248f33
David <roidealsinc@gmail.com>
2022-12-27 10:09:11 -0800

    chnage oriented

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void print_prime(long n);
+
+/**
+ * main - check code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_prime(612852475143);
+	return (0);
+}
+
+/**
+ * print_prime - find and print prime factor
+ * @n: variable
+ */
+void print_prime(long n)
+{
+	long i, j, k;
+
+	j = 1;
+	for (i = 2; i <= n; i++)
+	{
+		k = n / j;
+		if (k % i == 0)
+		{
+			j = j * i;
+			if (j == n)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+			else
+				i = 1;
+		}
+	}
+}
