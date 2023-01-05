b80376c2bb6f373ed5dc949464bbcb935d19bdef
David <roidealsinc@gmail.com>
2023-01-05 03:57:29 -0800

    change oriented

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,12 @@
  * factorial - claculate the factorial of a given number
  * @n: The number to be calculated
  *
- * Return
+ * Return: integer
  */
 int factorial(int n)
 {
 	if (n > 0)
-		return (n * factorial(n-1));
+		return (n * factorial(n - 1));
 	if (n == 0)
 		return (1);
 	else
