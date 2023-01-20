aae605bd718a85d39c8bfc0867a16d116f4e9297
David <roidealsinc@gmail.com>
2023-01-19 18:56:33 -0800

    change oriented

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,12 +2,12 @@
 
 /**
  * swap_int - swap values between two variable.
- * @a - the first variable.
- * @b - the second variable.
+ * @a: the first variable.
+ * @b: the second variable.
  */
 void swap_int(int *a, int *b)
 {
-	*b = *a;
-	*a = *b;
+	a = &b;
+	b = &a;
 }
 
