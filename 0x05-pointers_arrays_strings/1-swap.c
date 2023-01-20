a4a0b787787b8a635065045540753584ba4c997b
David <roidealsinc@gmail.com>
2023-01-19 18:41:58 -0800

    change oriented

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values between two variable.
+ * @a - the first variable.
+ * @b - the second variable.
+ */
+void swap_int(int *a, int *b)
+{
+	*b = &a;
+	*a = &b;
+}
+
