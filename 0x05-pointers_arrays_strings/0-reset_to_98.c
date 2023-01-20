6920df1c23a06302c89683112e721e8d103ad875
David <roidealsinc@gmail.com>
2023-01-19 18:36:07 -0800

    change oriented

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set an variable value to 98.
+ * @n: The variable to reset.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - print on character
+ * @p: the character to be printed out.
+ */
+void _putchar(char p)
+{
+	write(1, &p, 1);
+}
+
