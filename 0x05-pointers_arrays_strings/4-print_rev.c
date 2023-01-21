227b00a6dc0dfe6b245ab3bfbaf96af18a43264e
David <roidealsinc@gmail.com>
2023-01-21 02:20:51 -0800

    change oriented

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ * @str: Tge strign to be printed out.
+ */
+void _puts(char *str)
+{
+	long len = 0;
+
+	do {
+		if (*str == '\0')
+		{
+			break;
+		}
+		_putchar(str[len]);
+		len++;
+	} while (str[len]);
+	_putchar('\n');
+}
