7f11971cc3cf18eb4a9f7916f7fa5a50da1a4b04
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-24 14:37:31 +0100

    change oriented

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - print the string even index characters.
+ * @str: the required string to print out.
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] = '\0'; count++)
+	{
+		if (count % 2 == 0)
+			_putchar(str[count]);
+	}
+	_putchar('\n');
+}
