af31ae9d3dab67050f0fbf33d9b3511cc94b7d96
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-24 16:29:03 +0100

    change oriented

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,13 +6,13 @@
  * @a: The array.
  * @n: The numbers of element printed out.
  */
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int len;
 
 	for (len = 0; len < n; len++)
 	{
-		printf("%d, ", a[len]);
+		printf("%d, ", (a + len));
 	}
 	printf("\n");
 }
