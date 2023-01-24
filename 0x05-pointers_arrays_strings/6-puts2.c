73a28948993a8e8ef7fb3ad6f956d0d44a4e8311
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-24 14:41:52 +0100

    change oriented

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@ void puts2(char *str)
 {
 	int count;
 
-	for (count = 0; str[count] = '\0'; count++)
+	for (count = 0; str[count] != '\0'; count++)
 	{
 		if (count % 2 == 0)
 			_putchar(str[count]);
