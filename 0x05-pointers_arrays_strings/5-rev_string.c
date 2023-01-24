485042563a2668bc5a8672da7ef11b134ab4f4c0
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-24 13:46:47 +0100

    change oriented

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rev_string - reverse a given string.
@@ -9,17 +10,27 @@ void rev_string(char *s)
 	long len = 0;
 	long count;
 
+
 	for (count = 0; s[count] != '\0'; count++)
 	{
 		len++;
 	}
-	char rev_str[len];
-
-	for (count = 0; s[count] != '\0'; count++)
+	len = len - 1;
+	for (count = 0; len > count; count++)
 	{
+		swap_char(s + len, s + count);
 		len--;
-		rev_str[len] = s[count];
 	}
-	for (count = 0; s[count] != '\0'; count++)
-		s[count] = rev_str[count];
+}
+
+/**
+ * swp_char - swap character
+ * @s: The given string.
+ * @rev_s: The reversed string.
+ */
+void swap_char(char *s, char *rev_s)
+{
+	char swap = *rev_s;
+	*rev_s = *s;
+	*s = swap;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void swap_char(char *s, char *r);
 #endif
 
