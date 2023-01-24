870c3311784ebee59beb01077ad9468780bf33d2
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-24 15:50:03 +0100

    change oriented

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,20 +6,20 @@
  */
 void puts_half(char *str)
 {
-        int len;
+	int len;
 
-        len = _strlen(str) - 1;
-        if (len % 2 == 0)
-                len = len / 2;
-        else
-                len = (len - 1) / 2;
-        do {
-                if (str[len] != 0)
-                        _putchar(str[len]);
-                else
-                        break;
-        } while (len++);
-        _putchar('\n');
+	len = _strlen(str) - 1;
+	if (len % 2 == 0)
+		len = len / 2;
+	else
+		len = (len - 1) / 2;
+	do {
+		if (str[len] != 0)
+			_putchar(str[len]);
+		else
+			break;
+	} while (len++);
+	_putchar('\n');
 }
 
 /**
