3c6fae8d2888813c3d05df5a4ede3176641f3bf3
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-25 16:27:43 +0100

    change oriented

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,43 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcpy - copy a string to another string.
+ * _strcat - concatenates two strings.
  * @src: The source string or initial string.
  * @dest: The destination of the copir=ed string.
  *
  * Return: dest.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int len = 0;
+	int lendest = 0;
+	int i = 0;
 
-	if (*src == '\0')
-		*dest = *src;
-	else
+	for (lendest = _strlen(dest); lendest <= (_strlen(dest) + _strlen(src)); lendest++)
 	{
-		do {
-			*(dest + len) = *(src + len);
-			len++;
-		} while (src[len] != '\0');
-
-		*(dest + len) = '\0';
+			*(dest + lendest) = *(src + i);
+			i++;
 	}
-
 	return (dest);
+}
+
+/**
+ * _strlen - measure tht length of string
+ * @s: the string yo be measured.
+ *
+ * Return: the lenght of string.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	do {
+		if (*s == '\0')
+			break;
+
+		len++;
+	} while (s[len]);
+
+	return (len);
 }
