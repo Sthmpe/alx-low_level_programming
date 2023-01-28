d0cc8ea8f15ec27789560fb189f3e3ae4dad64fd
Sthmpe <roidealsinc@gmail.com>
2023-01-28 11:35:10 -0500

    change oriented

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,8 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; index < n && src[index] != '\0'; index++)
 		dest[index] = src[index];
 
-	if (src[index] == '\0')
-		dest[index] = src[index];
+	for (; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
