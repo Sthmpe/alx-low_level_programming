c6e98a18203c8a67e8b0d315b2cde38cfb39427f
Sthmpe <roidealsinc@gmail.com>
2023-01-28 10:00:48 -0500

    change oriented

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a n of character from a src string to a dest string.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The number of string element or index to copy.
+ *
+ * Return: dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index]; index++)
+		*(dest + index) = *(src + index);
+	return (dest);
+}
