a240f2ae2d713c92270000b639659724061ea09d
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-31 16:27:20 +0100

    change oriented

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - Encode a string by rotating by 13 places
+ * @s: The string to encode.
+ *
+ * Return: The encoded the string.
+ */
+char *rot13(char *s)
+{
+	int len = 0, index;
+	char rot[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rev[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwzyxabcdefghijklm";
+
+	for (; s[len]; len++)
+	{
+		for (index = 0; index <= 51; index++)
+		{
+			if (s[len] == rot[index])
+			{
+				s[len] = rev[index];
+				break;
+			}
+		}
+
+	}
+
+	return (s);
+}
