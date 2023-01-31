1e2838388cebfbaa8ae981c695c9f66e763d5006
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-31 15:33:22 +0100

    change oriented

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - Encode a string
+ * @s: the string to be encoded.
+ *
+ * Return: The encoded string.
+ */
+char *leet(char *s)
+{
+	int len = 0;
+	int index;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (s[++len])
+	{
+		for (index = 0; leet[index]; index++)
+		{
+			if (s[len] == leet[index] ||
+			s[len] - 32 == leet[index])
+				s[len] = index + '0';
+		}
+	}
+	return (s);
+}
