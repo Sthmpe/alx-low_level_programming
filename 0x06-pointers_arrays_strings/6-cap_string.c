46986765d6870fc139d3a89884f9fe8f1c0fd736
Davidunilorin <olaniteoluwasegun5@gmail.com>
2023-01-31 13:51:52 +0100

    change oriented

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,31 +13,33 @@ char *cap_string(char *s)
 
 	for (; s[len]; len++)
 	{
-		if (s[len] == ' ' && s[len + 1] >= 'a' && s[len] <= 'z')
+		if (s[len] == ' ' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '\t' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '\t' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '\n' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '\n' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == ',' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == ',' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == ';' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == ';' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '.' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '.' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '!' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '!' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '"' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '"' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '(' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '(' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == ')' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == ')' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '{' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '{' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '}' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '}' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
-		else if (s[len] == '?' && s[len + 1] >= 'a' && s[len] <= 'z')
+		else if (s[len] == '?' && s[len + 1] >= 'a' && s[len + 1] <= 'z')
+			s[len + 1] =  s[len + 1] - 32;
+		else if (len == 0 && s[len] >= 'a' && s[len] <= 'z')
 			s[len + 1] =  s[len + 1] - 32;
 	}
 	return (s);
