5c4a60799527711ef5325ad0b7958e3353165535
Sthmpe <roidealsinc@gmail.com>
2023-02-19 10:00:57 -0500

    change oriented

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -8,18 +9,14 @@
  */
 int main(void)
 {
-    int c3[2][2] = {
-        {0, 1},
-        {10, 11},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 2);
-    print_diagsums((int *)c5, 5);
+    char *s = "hello";
+    char *f;
+
+    f = strchr(s, 'h');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
     return (0);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,9 +14,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] == c)
-		{
 			return (s + i);
-		}
 	}
 	return (NULL);
 }
