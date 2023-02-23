adc36ac7d26969cc4be3a07ade78ace87c797085
Sthmpe <roidealsinc@gmail.com>
2023-02-23 07:07:22 -0500

    change oriented

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,38 +1,37 @@
 #include "main.h"
-
 /**
-* str_concat - a function that concatenates two strings.
-*@s1:First string
-*@s2:Second string
-*
-*Return: NULL in case of failure , but pointer to new string in
-*case of success
-*/
-
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer  to the newly allocated space in memory which contains
+ *         the content of s1, followed by the content of s2
+ *         if fail to allocate return NULL.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int index, concat_index = 0,  len = 0;
+	int i, index1 = 0, index2 = 0;
+	char *str_concat;
 
-	if (s1 == NULL)
-		s1 = "";
+	for (i = 0; s1[i]; i++)
+		index1++;
+	for (i = 0; s2[i]; i++)
+		index2++;
 
-	if (s2 == NULL)
-		s2 = "";
+	str_concat = malloc(sizeof(char) * (index1 + index2 + 1));
 
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+	if (s1 == NULL && s2 == NULL)
+	{
+		str_concat = '\0';
+		return (str_concat);
+	}
+	else if (str_concat ==  NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	for (i = 0; i <= index1; i++)
+		str_concat[i] = s1[i];
+	for (i = 0; i <= index2 + 1; i++)
+		str_concat[index1++] = s2[i];
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-
-	return (concat_str);
+	return (str_concat);
 }
