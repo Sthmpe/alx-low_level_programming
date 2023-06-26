bed90cf3fd94ae72a1e382ac8ef903b85f475547
David olanite <olaniteolanight@gmail.com>
2023-06-26 11:06:29 +0100

    change oriented

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - print the name of the file compiled.
- * Rturn: 0.
+ * Return: 0.
  */
 int main(void)
 {
-        printf("%s\n", __BASE_FILE__);
-        return (0);
+	printf("%s\n", __BASE_FILE__);
+	return (0);
 }
+
